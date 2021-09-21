1b698abeb1ee653f2cdaa8625b0e7c4a035715b0
MrWrynn <64748923+MrWrynn@users.noreply.github.com>
2021-09-21 13:47:15 -0600

    Add files via upload

--- Dado.java
+++ Dado.java
@@ -4,6 +4,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import java.lang.NullPointerException;
 
 
 public class Dado {
@@ -19,31 +20,39 @@ public class Dado {
         ActionListener oyente=new ActionListener(){
             @Override
         public void actionPerformed(ActionEvent e) { //0 es reto, 1 es trampa y 2 es tunel
-            int i=1;
-            int n=1;
-            int num=1;
-            moverse=generador.nextInt(4)+1;
-            JOptionPane.showMessageDialog(null,"Avanza: "+moverse+" casillas");
-            tablero.recorrerDado(moverse);
-            System.out.println(tablero.runner.getData());
-            int runnerA = (int) tablero.runner.getData();
-            int movimiento=generador.nextInt(3)+1;
-            while (n==1){
-                System.out.println("Mov: "+movimiento);
-                if (runnerA==1 && num<=movimiento){
-                    tablero.runner=tablero.runner.anterior;
-                    System.out.println("retrocede");
-                    num++;
-                } else if(runnerA==2 && num<=movimiento){
-                    tablero.runner=tablero.runner.siguiente;
-                    System.out.println("avanza");
-                    num++;
+            
+            try{ 
+                int n=1;
+                int num=1;
+                Reto reto = new Reto();
+                moverse=generador.nextInt(4)+1;
+                JOptionPane.showMessageDialog(null,"Avanza: "+moverse+" casillas");
+                tablero.recorrerDado(moverse);
+                System.out.println(tablero.runner.getData());
+                int runnerA = (int) tablero.runner.getData();
+                int movimiento=generador.nextInt(3)+1;
+                while (n==1){
+                    System.out.println("Mov: "+movimiento);
+                    if (runnerA==1){
+                        JOptionPane.showMessageDialog(null, "Has caído en una trampa, retrocedes "+movimiento+" casillas");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Has caído en un tunel, avanzas "+movimiento+" casillas");
+                    }
+                    while (runnerA==1 && num<=movimiento){
+                        tablero.runner=tablero.runner.anterior;
+                        num++;
+                    } while (runnerA==2 && num<=movimiento){
+                        tablero.runner=tablero.runner.siguiente;
+                        num++;
+                    }
+                    if (runnerA==0){
+                        //reto.reto();
+                        tablero.runner=tablero.runner.siguiente;
+                    }
+                    n++;
                 }
-                n++;
-            }
-            System.out.println(tablero.runner.getData());
-            //tablero.runner=tablero.runner.anterior;
-            System.out.println("*"+tablero.runner.getData());
+                System.out.println("*"+tablero.runner.getData());
+            }catch(java.lang.NullPointerException ex){JOptionPane.showMessageDialog(null, "Has ganado");}
         }
             
         };
