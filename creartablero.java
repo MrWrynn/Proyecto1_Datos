ad5faee7c89b5b6d66d9f505f558d54a4638103d
eduardocv30 <jecruz@estudianTEC.cr>
2021-10-05 14:09:06 -0600

    Update creartablero.java

--- creartablero.java
+++ creartablero.java
@@ -1,5 +1,7 @@
 package Proyecto1.Proyecto1;
 import java.util.Random;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class creartablero {
     Random generador=new Random();
@@ -7,59 +9,52 @@ public class creartablero {
     Trampa tp=new Trampa();
     Tunel tn=new Tunel();
     Casilla inicio=new Casilla();
-    int[] arreglo=new int[16];
+    JButton botones[]=new JButton[16];
     int i=1;
     int r=1;
     int tr=1;
     int tu=1;
 
     public creartablero(Ventana ventana,LinkedList tablero){
-        inicio.crear(ventana);
         tablero.insertNode(3);
-        arreglo[0]=3;
+        botones[0]=new JButton("INICIO");
+        botones[0].setBackground(Color.GRAY);
+        botones[0].setEnabled(false);
+        ventana.add(botones[0]);
         while (i <=14){
             int opcion=generador.nextInt(3);
             if ((opcion==0) && (r<=7)){
                 tablero.insertNode(0);
-                cr.crear(ventana);
-                arreglo[i]=0;
+                botones[i]=new JButton();
+                botones[i].setBackground(Color.blue);
+                botones[i].setEnabled(false);
+                ventana.add(botones[i]);
                 i++;
                 r++;
             }
             else if ((opcion==1) && (tr<=3)){
                 tablero.insertNode(1);
-                tp.crear(ventana);
-                arreglo[i]=1;
+                botones[i]=new JButton();
+                botones[i].setBackground(Color.red);
+                botones[i].setEnabled(false);
+                ventana.add(botones[i]);
                 i++;
                 tr++;
             }
             else if ((opcion==2) && (tu<=4)){
                 tablero.insertNode(2);
-                tn.crear(ventana);
-                arreglo[i]=2;
+                botones[i]=new JButton();
+                botones[i].setBackground(Color.green);
+                botones[i].setEnabled(false);
+                ventana.add(botones[i]);
                 i++;
                 tu++;}}
-        inicio.crear(ventana);
+        botones[15]=new JButton();
+        botones[15].setBackground(Color.GRAY);
+        botones[15].setEnabled(false);
+        ventana.add(botones[15]);
         tablero.insertNode(3);
-        arreglo[15]=3;
     }
-    public creartablero(Ventana ventana,LinkedList tablero,int[]arreglo){
-        for (int i:arreglo){
-            switch(i){
-                case 0:tablero.insertNode(0);
-                        cr.crear(ventana);
-                        break;
-                case 1:tablero.insertNode(1);
-                        tp.crear(ventana);
-                        break;
-                case 2:tablero.insertNode(2);
-                        tn.crear(ventana);
-                        break;
-                case 3:tablero.insertNode(3);
-                        inicio.crear(ventana);
-            }
-        }
-    }
-
+    
 
 }
